29aabed3d3b07e29d14d9be38eb2c41cff77d5a3
Joo Seu Yoon <ajanajan1217@gmail.com>
2022-04-04 02:53:33 +0900

    get words data

--- assignment02/get_movie_script_map.py
+++ assignment02/get_movie_script_map.py
@@ -5,6 +5,7 @@ crime_movie = {}
 romance_movie = {}
 
 def movie_list(genre):
+    words_dict = {}
 
     for mvName in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/inspect_movie_scripts/'+genre+'_movies'):
         if(mvName != '.DS_Store') :
@@ -13,9 +14,10 @@ def movie_list(genre):
                 item = item.strip()
                 words = re.split('/', item)
                 if(len(words[0]) > 0) :
-                    (globals()['{}_movie'.format(genre)])[mvName[10:len(mvName)-4]] = {words[0] : int(words[1])}
-                    
+                    #(globals()['{}_movie'.format(genre)]) = {(mvName[10:len(mvName)-4]): {words[0] : int(words[1])}}
+                    words_dict[words[0]] = int(words[1])
+            globals()["{}_movie".format(genre)][mvName[10:len(mvName)-4]] = words_dict        
             mvFile.close()
-    return globals()['{}_movie'.format(genre)]
+    return globals()["{}_movie".format(genre)]
 
-print(movie_list("romance"))
+print((movie_list("romance").keys()))
