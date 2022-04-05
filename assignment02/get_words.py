02ff5bdadbf4d2808887dd5036eaa34a4273453a
Joo Seu Yoon <ajanajan1217@gmail.com>
2022-04-06 04:38:09 +0900

    invert index implementation

--- assignment02/get_words.py
+++ assignment02/get_words.py
@@ -1,34 +1,42 @@
 import os
 import re
 
-crime_movie = {}
-romance_movie = {}
+class invert_index:
+    def get_stop_words():
+        stopwords = {}
+        stopword_file = open(os.path.dirname(os.path.abspath(__file__)) + '/words/stop_words.txt')
+        for stop_word in stopword_file.readlines():
+            stopwords[stop_word[0:len(stop_word)-1]] = 1
+        return stopwords
 
-class get_words:
-
-    def movie_dict(genre):
-
-        words_dict = {}
-
-        for mvName in os.listdir(os.path.dirname(os.path.abspath(__file__)) + '/inspect_movie_scripts/'+genre+'_movies'):
-            if(mvName != '.DS_Store') :
-                mvFile = open(os.path.dirname(os.path.abspath(__file__)) + '/inspect_movie_scripts/'+genre+'_movies/'+mvName, 'r')
-                for item in mvFile.readlines() :
-                    item = item.strip()
-                    words = re.split('/', item)
-                    if(len(words[0]) > 0) :
-                        #(globals()['{}_movie'.format(genre)]) = {(mvName[10:len(mvName)-4]): {words[0] : int(words[1])}}
-                        words_dict[words[0]] = int(words[1])
-                globals()["{}_movie".format(genre)][mvName[10:len(mvName)-4]] = words_dict        
-                mvFile.close()
-        return globals()["{}_movie".format(genre)]
-
-    def bad_words_list() :
-        bad_words = []
-        bad_words_file = open(os.path.dirname(os.path.abspath(__file__)) + '/words/bad_words.txt', 'r')
-        for item in bad_words_file.readlines() :
-            item = item.strip()
-            bad_words.append(item)
-        bad_words_file.close
+    def movie_dict(movie_name):
+        try:
+            script_file = open(os.path.dirname(os.path.abspath(__file__)) + '/movies/' + movie_name + '.txt', 'r')
+            script_words = {}
+            
+            for script_line in script_file.readlines():
+                if(script_line != ''):
+                    for word in re.split('[\s+\d`_,.?!&//@$#*:;()"\'\- ]', script_line) :
+                        word = word.lower()
+                        if (word not in invert_index.get_stop_words()) :
+                            if(word not in script_words) :
+                                script_words[word] = 1
+                            else :
+                                script_words[word] += 1
+            script_words = sorted(script_words.items())
+            script_file.close()
+            return script_words
+        except:
+            print("execption error")
     
-        return bad_words
+    def get_all_movies():
+        movie_terms = {}
+        try:
+            for mvName in os.listdir('movies/') : 
+                if(mvName != '.DS_Store') :
+                    mvName = mvName[0:len(mvName) - 4]
+                    print('Invert Indexing movie  ' + mvName.replace('-',' '))
+                    movie_terms[mvName] = invert_index.movie_dict(mvName)
+            return movie_terms
+        except:
+            print("exception error")
