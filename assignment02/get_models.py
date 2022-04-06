d3ce646e90ab3974f439533678782bd35b5e67bc
Joo Seu Yoon <ajanajan1217@gmail.com>
2022-04-06 17:36:24 +0900

    boolean model complete

--- assignment02/get_models.py
+++ assignment02/get_models.py
@@ -1,40 +1,68 @@
 import get_words
+from collections import defaultdict
 
 class get_models:
 
     def make_boolean_model(words_list, operators_list):
         invert_result = get_words.invert_index.movie_dict()
-        boolean_model = {str : {}}
+        boolean_model = defaultdict(dict)
         boolean_result = {}
         movie_names = get_words.invert_index.movie_names()
+        boolmodel = 0
         
         for words in words_list :
             if words.split(' ')[0] == 'not' :
                 if words.split(' ')[1] in invert_result.keys() :
                     for mvName in movie_names : 
-                        boolmodel = {mvName : 0}
-                        if invert_result[words.split(' ')[1]][mvName] < 1 :
-                            boolmodel = {mvName : 1}
-                        else:
-                            boolmodel = {mvName : 0}
-                        boolean_model[words] = boolmodel
-                    
+                        if mvName in invert_result[words.split(' ')[1]] : 
+                            if invert_result[words.split(' ')[1]][mvName] < 1 :
+                                boolmodel = 1
+                            else:
+                                boolmodel = 0
+                            boolean_model[words.split(' ')[1]][mvName] = boolmodel
+                        else :
+                            boolmodel = 1
+                            boolean_model[words.split(' ')[1]][mvName] = boolmodel
+                else : 
+                    boolmodel = 1
+                    boolean_model[words.split(' ')[1]] = {mvName : boolmodel}
             else :
                 for mvName in movie_names : 
                     if words in invert_result.keys() : 
-                        if invert_result[words][mvName] < 1 :
-                            boolmodel = {mvName : 0}
-                        else : 
-                            boolmodel = {mvName : 1}
+                        if mvName in invert_result[words].keys() : 
+                            if invert_result[words][mvName] < 1 :
+                                boolmodel = 0
+                            else : 
+                                boolmodel = 1
+                            boolean_model[words][mvName] = boolmodel
+                        else :
+                            boolmodel = 0
+                            boolean_model[words][mvName] = boolmodel
                     else :
-                        boolmodel = {mvName : 0}
-
-                    boolean_model[words] = boolmodel
+                        boolmodel = 0
+                        boolean_model[words][mvName] = boolmodel
+                    
 
         #result 만들기 operators list로
-        
 
-        return boolean_model
+        boolean_result = boolean_model.copy()
+
+        i = 0
+        for i in range(len(operators_list)):
+            if operators_list[i] == 'and' :
+                for mvName in movie_names:
+                    if (boolean_model[list(boolean_model)[i]][mvName] == 1 and boolean_model[list(boolean_model)[i + 1]][mvName] == 1) :
+                        boolean_model[list(boolean_model)[i + 1]][mvName] = 1
+                    else :
+                        boolean_model[list(boolean_model)[i + 1]][mvName] = 0
+            elif operators_list[i] == 'or' :
+                for mvName in movie_names:
+                    if (boolean_model[list(boolean_model)[i]][mvName] == 1 or boolean_model[list(boolean_model)[i + 1]][mvName] == 1) :
+                        boolean_model[list(boolean_model)[i + 1]][mvName] = 1
+                    else :
+                        boolean_model[list(boolean_model)[i + 1]][mvName] = 0
+        
+        return boolean_model, boolean_result
 
     def make_vector_space_model():
         
--- assignment02/show_models.py
+++ assignment02/show_models.py
@@ -1,17 +1,11 @@
 import plotly.graph_objects as go
 import get_models
+import get_words
 
-#def make_table(user_words_list, operator_list):
-    
-
-
-
-def main() :
+def make_boolean_model_table(user_query):
     user_operator_list = []
     user_words_list = []
-    user_query = input("Input your word with query.\n(Ex. yellow AND white)\nQuery : ")
-    print("\n If you input wrong query program cannot make table.")
-
+    table_query = user_query
     user_query = user_query.split(" ")
     i = 0
     while i < len(user_query) :
@@ -23,8 +17,50 @@ def main() :
         else : 
             user_words_list.append(user_query[i].lower())
         i+=1
-    print(get_models.get_models.make_boolean_model(user_words_list, user_operator_list))
     
+    boolean_model, boolean_result = get_models.get_models.make_boolean_model(user_words_list, user_operator_list)
+    
+    headerList = ["Movie Name"]
+    for word in user_words_list:
+        headerList.append(word)
+    headerList.append(table_query)
+    
+    cellList = []
+
+    cellList.append(get_words.invert_index.movie_names())
+
+    for key in boolean_model.keys():
+        cellList.append(list(boolean_model[key].values()))
+    
+    cellList.append(list(boolean_result[list(boolean_result)[len(boolean_result) - 1]].values()))
+            
+
+    fig = go.Figure(data=[go.Table(
+        header=dict(values = headerList,
+                    line_color = 'white',
+                    fill_color = 'yellow',
+                    align = 'center'),
+        cells=dict(values = cellList,
+                    line_color = 'white',
+                    fill_color = 'white',
+                    align = 'center'))
+    ])
+
+    fig.update_layout(width=1000,height=400)
+    fig.show()
+    
+
+
+
+def main() :
+    user_query = input("Input your word with query.\n(Ex. yellow AND white)\nQuery : ")
+    print("\n If you input wrong query program cannot make table.")
+
+    make_boolean_model_table(user_query)
+    
+
+
+
 
 if __name__ == "__main__":
     main()
