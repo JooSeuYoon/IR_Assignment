93073deba3a041fbb53bc08e5c8fd0a9c9c796c2
Joo Seu Yoon <ajanajan1217@gmail.com>
2022-04-07 18:10:57 +0900

    vector space model implements

--- assignment02/get_models.py
+++ assignment02/get_models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import get_words
 from collections import defaultdict
 
@@ -52,20 +53,58 @@ class get_models:
             if operators_list[i] == 'and' :
                 for mvName in movie_names:
                     if (boolean_model[list(boolean_model)[i]][mvName] == 1 and boolean_model[list(boolean_model)[i + 1]][mvName] == 1) :
-                        boolean_model[list(boolean_model)[i + 1]][mvName] = 1
+                        boolean_result[list(boolean_model)[i + 1]][mvName] = 1
                     else :
-                        boolean_model[list(boolean_model)[i + 1]][mvName] = 0
+                        boolean_result[list(boolean_model)[i + 1]][mvName] = 0
             elif operators_list[i] == 'or' :
                 for mvName in movie_names:
                     if (boolean_model[list(boolean_model)[i]][mvName] == 1 or boolean_model[list(boolean_model)[i + 1]][mvName] == 1) :
-                        boolean_model[list(boolean_model)[i + 1]][mvName] = 1
+                        boolean_result[list(boolean_model)[i + 1]][mvName] = 1
                     else :
-                        boolean_model[list(boolean_model)[i + 1]][mvName] = 0
+                        boolean_result[list(boolean_model)[i + 1]][mvName] = 0
         
         return boolean_model, boolean_result
 
-    def make_vector_space_model():
+    def make_vector_space_model(query_list):
         
         vector_space_model = {}
+        mvNameList = get_words.invert_index.movie_names()
 
+        movie_term_list = get_words.invert_index.movie_dict()
+        tf = {str : {str : int}}
+        df = {str : int}
+        idf = {str : int}
+        weight = {str : {str : int}}
+        
+        cosine_score = {str : int}
+
+        #document frequency & inverse
+        #term freauency & normalize 
+        for term in query_list:
+            df[term] = 0
+            tf[term] = {}
+            if(term in movie_term_list.keys()):
+                for mvName in mvNameList :
+                    if(mvName in movie_term_list[term].keys()) :
+                        df[term] += 1
+                        tf[term] = {mvName : (movie_term_list[term][mvName])}
+                    else :
+                        tf[term] = {mvName : 0}
+            #inverse 구하기
+            idf[term] = np.log10(len(mvNameList) / df[term]) 
+        
+        #TF.IDF Weighting
+        for term in query_list :
+            weight[term] = {}
+            for mvName in mvNameList :
+                weight[term] = {mvName : np.log10(1 + tf[term][mvName]) * idf[term]}
+                cosine_score[mvName] += df[term] * weight[term][mvName]
+        
+            
+        #cosine score
+        for mvName in mvNameList:
+            cosine_score[mvName] =  cosine_score[mvName] / len(mvNameList)
+
+        
+        
         return vector_space_model
